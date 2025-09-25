d987df02e710051a29618c081048af0c4387d0c8
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2025-09-25 13:20:57 -0400

    aot script stops

--- aot.cmd
+++ aot.cmd
@@ -1,5 +1,7 @@
 del Tests\Synqra.Tests\bin\Release\net9.0\win-x64\publish\Synqra.Tests.exe
 dotnet publish Tests/Synqra.Tests -c Release -r win-x64 -f net9.0
+if errorlevel 1 exit /b 1
 ::Tests\Synqra.Tests\bin\Release\net9.0\win-x64\publish\Synqra.Tests.exe --treenode-filter /*/*/PerformanceTests/*
 ::Tests\Synqra.Tests\bin\Release\net9.0\win-x64\publish\Synqra.Tests.exe 
 Tests\Synqra.Tests\bin\Release\net9.0\win-x64\publish\Synqra.Tests.exe --treenode-filter "/*/*/*[(Ca1tegory!=Performance)&(CI!=false)]/*[(Ca2tegory!=Performance)&(CI!=false)]"
+if errorlevel 1 exit /b 1
