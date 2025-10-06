a3ef1956fcca3a5f9107979ba9888925b944559a
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2025-10-06 13:23:34 -0400

    Improve Emergency Logger

--- Synqra.Utils/EmergencyLog.cs
+++ Synqra.Utils/EmergencyLog.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using System.Security.AccessControl;
 using System.Security.Principal;
+using System.Text;
 using System.Threading;
 
 namespace Synqra;
@@ -12,6 +14,10 @@ namespace Synqra;
 /// Every call opens a handle to append to a log file in the temp folder.
 /// No flushing, no caching, no batching.
 /// It is not performance efficient but reliable and always ready.
+/// It makes 1MiB file size limit and rolls over between multiple files.
+/// It keeps a logs for 1 week.
+/// It holds 100 files maximum (100MiB total) but expected to cleanup older files sooner.
+/// The encoding is utf8 (no_bom)
 /// </summary>
 public class EmergencyLog
 {
@@ -19,20 +25,31 @@ public class EmergencyLog
 
 	private EmergencyLog()
 	{
-		_logFilePath = Path.Combine(Path.GetTempPath(), "SynqraEmergency.log");
-		// _logFilePath2 = Path.Combine(Path.GetTempPath(), "SynqraEmergency.bak");
 		if (!AppContext.TryGetSwitch("Synqra.EmergencyLog.Enabled", out var isEnabled))
 		{
 			isEnabled = true;
 		}
-		if (!isEnabled)
+		if (isEnabled)
 		{
-			_logFilePath = string.Empty;
+			var dir = Path.Combine(Path.GetTempPath(), "Synqra");
+			Directory.CreateDirectory(dir);
+			_logFilePath = Path.Combine(dir, "Emergency.log");
+			_logFilePathTemplate = Path.Combine(dir, "Emergency_{0}.log");
+			var mutexName = _logFilePath.ToUpperInvariant();
+			foreach (var c in Path.GetInvalidFileNameChars())
+			{
+				mutexName = mutexName.Replace(c, '_');
+			}
+			// mutex name uniquiness should corelate with temp folder uniquiness.
+			// When TMP configured per-user, or per-session, or per-machine same would ne with mutex name
+			_mutexName = $"Global\\SynqraEmergencyLog_{mutexName}";
+			// _mutexName = $"Global\\SynqraEmergencyLog";
 		}
 	}
 
-	private string _logFilePath;
-	// private string _logFilePath2;
+	private string? _logFilePath;
+	private string? _mutexName;
+	private string? _logFilePathTemplate;
 
 	/// <summary>
 	/// Conditional message - #if DEBUG
@@ -54,67 +71,171 @@ public class EmergencyLog
 
 	public void Message(string message)
 	{
-		if (_logFilePath == string.Empty)
+		try
 		{
-			return;
-		}
-		// TODO use \\global and ACL on windows (or don't... TMP is session specific anyway. UPD: No, I just set both system and user tmp to C:\Dev\Temp and now I have services vs user going to same file. Mutext needs to be global on Windows)
-		// TODO do something for browser
-		var line = $"[{DateTime.UtcNow:o}] {message}{Environment.NewLine}";
+			if (_logFilePath == null || _logFilePathTemplate == null)
+			{
+				return;
+			}
+			// TODO use \\global and ACL on windows (or don't... TMP is session specific anyway. UPD: No, I just set both system and user tmp to C:\Dev\Temp and now I have services vs user going to same file. Mutext needs to be global on Windows)
+			// TODO do something for browser
+			message = $"[{DateTimeOffset.Now:yyyy'-'MM'-'dd' 'HH':'mm':'ss'.'fffffffzzz}] {message}{Environment.NewLine}";
 
-		Mutex mutex = null;
+			Mutex mutex = null;
 #if NET8_0_OR_GREATER
-		if (OperatingSystem.IsWindows())
-		{
-			var everyone = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
-			var rule = new MutexAccessRule(everyone, MutexRights.FullControl, AccessControlType.Allow);
-
-			var security = new MutexSecurity();
-			security.AddAccessRule(rule);
-
-			bool createdNew;
-			mutex = MutexAcl.Create(
-				initiallyOwned: false,
-				name: "Global\\SynqraEmergencyLog",
-				createdNew: out createdNew,
-				mutexSecurity: security);
-
-		}
-		else if (OperatingSystem.IsBrowser())
-		{
-			Console.WriteLine("⚠️🚨 SynqraEmergencyLog: " + line);
-			return;
-		}
-#endif
-		if (mutex == null)
-		{
-			mutex = new Mutex(false, "Global\\SynqraEmergencyLog");
-		}
-		using (mutex)
-		{
-			try
+			if (OperatingSystem.IsWindows())
 			{
-				mutex.WaitOne();
-				var fi = new FileInfo(_logFilePath);
-				if (fi.Exists && fi.Length > 1024 * 1024)
+				try
 				{
-					fi.Delete();
-					/*
-					Message("[System] Previous log file exceeded 1MiB and deleted");
-					var fi2 = new FileInfo(_logFilePath2);
-					if (fi2.Exists)
+					var everyone = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
+					var security = new MutexSecurity();
+					security.AddAccessRule(new MutexAccessRule(everyone, MutexRights.Modify | MutexRights.Synchronize, AccessControlType.Allow));//| MutexRights.ReadPermissions | MutexRights.TakeOwnership
+					var self = WindowsIdentity.GetCurrent().User;
+					if (self != null)
 					{
-						fi2.Delete();
+						security.AddAccessRule(new MutexAccessRule(self, MutexRights.FullControl, AccessControlType.Allow));
 					}
-					fi.MoveTo(_logFilePath2);
-					*/
+
+					bool createdNew;
+					mutex = MutexAcl.Create(
+						initiallyOwned: false,
+						name: _mutexName,
+						createdNew: out createdNew,
+						mutexSecurity: security);
 				}
-				File.AppendAllText(_logFilePath, line);
+				catch
+				{
+					// It will fall back to general approach below. Unfortunately, can't log this here to avoid loops.
+#if DEBUG
+					// WARNING! If you got exception and break here - take a good care about the cause, think it thoroughly, there are zero exceptions expected.
+					Debugger.Break();
+					throw;
+#endif
+				}
 			}
-			finally
+			else if (OperatingSystem.IsBrowser())
 			{
-				mutex.ReleaseMutex();
+				Console.WriteLine("⚠️🚨 SynqraEmergencyLog: " + message);
+				return;
 			}
+#endif
+			if (mutex == null)
+			{
+				mutex = new Mutex(false, _mutexName);
+			}
+			using (mutex)
+			{
+				bool acquired = false;
+				try
+				{
+					acquired = mutex.WaitOne();
+				}
+				catch (AbandonedMutexException)
+				{
+					// The mutex was abandoned in another process, it will still get acquired. No need to do anything special here, it is normal case.
+					acquired = true;
+				}
+				try
+				{
+					var fi = new FileInfo(_logFilePath);
+					if (fi.Exists && fi.Length > 1024 * 1024)
+					{
+						try
+						{
+							/*
+							 Here we go - the rollover logic!
+							 Honestly it does not feel fair that EmergencyLog is not as reliable as it claims. It should be as a black box for
+							 flight crash research. Chances are, people log things that can happen once a month mysteriously and hard to reproduce...
+							 And what you will say? File is too big and have to be deleted?
+							 2025-10-05 I decided to change my mind and implement a simple rollover between 2 files instead of deleting the log file.
+							*/
+
+							/* V1 [Problem: can lose some data randomly because of roll over]
+							fi.Delete();
+							Message("[System] Previous log file exceeded 1MiB and deleted");
+							*/
+
+							/* V2 1 bak file [Problem: can lose some data if there are a lot of rapid events. Need a time-based fix]
+							var fi2 = new FileInfo(_logFilePath2);
+							if (fi2.Exists)
+							{
+								fi2.Delete();
+							}
+							fi.MoveTo(_logFilePath2);
+							*/
+
+							// V3 Log4Net style rename chain
+							var now = DateTime.UtcNow;
+							float maxFile = 0;
+							const int MaxTotalFiles = 100;
+							string DealWithNextFile(int number)
+							{
+								maxFile = number;
+								var file = string.Format(_logFilePathTemplate, number);
+								var fi = new FileInfo(file);
+								if (fi.Exists)
+								{
+									bool needToDrop = (now - fi.LastWriteTimeUtc).TotalDays > 7 || number > MaxTotalFiles; // this must have at least some limit, and 100 files looks like way too much. Having said that it is just 100MB (1MB per file), so it could make sense to increase 1 file size.
+									var next = DealWithNextFile(number + 1);
+									if (needToDrop)
+									{
+										fi.Delete();
+									}
+									else
+									{
+										fi.MoveTo(next);
+									}
+								}
+								return file;
+							}
+							fi.MoveTo(DealWithNextFile(2));
+							Message($"[EmergencyLog] Previous log file exceeded 1MiB and renamed. It is {maxFile} files a week ({maxFile / MaxTotalFiles:P0} capacity)");
+						}
+						catch
+						{
+#if DEBUG
+							Debugger.Break();
+							throw;
+#endif
+						}
+					}
+					FileAppendAllText(_logFilePath, message);
+				}
+				finally
+				{
+					if (acquired)
+					{
+						mutex.ReleaseMutex();
+					}
+				}
+			} // Dispose() happens here, after ReleaseMutex()
 		}
+		catch
+		{
+			// Guaranteed no impact on the main application
+#if DEBUG
+			// WARNING! If you got exception and break here - take a good care about the cause, think it thoroughly, there are zero exceptions expected.
+			Debugger.Break();
+			throw;
+#endif
+		}
+	}
+
+	private void FileAppendAllText(string fileName, string message)
+	{
+		using var file = File.Open(fileName, FileMode.Append, FileAccess.Write, FileShare.Read);
+		using var sw = new StreamWriter(file);
+		/*
+		using var sw2 = new StreamWriter(fileName, new FileStreamOptions
+		{
+			Access = FileAccess.Write,
+			Mode = FileMode.Append,
+			PreallocationSize = 1024 * 1024,
+			Share = FileShare.Read,
+			// Options = FileOptions.SequentialScan,
+			// BufferSize = 1024 * 1024,
+		});
+		*/
+		sw.Write(message);
 	}
 }
--- Tests/Synqra.Tests/EmergencyLoggerTests.cs
+++ Tests/Synqra.Tests/EmergencyLoggerTests.cs
@@ -1,9 +1,12 @@
 ﻿using Synqra.Tests.TestHelpers;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TUnit.Assertions.AssertionBuilders.Wrappers;
 
 namespace Synqra.Tests;
 
@@ -24,6 +27,58 @@ internal class EmergencyLoggerTests : BaseTest
 	{
 		var keyData = Guid.NewGuid().ToString();
 		EmergencyLog.Default.Message("Should_save_emergency_log test " + keyData);
-		await Assert.That(File.ReadAllText(Path.Combine(Path.GetTempPath(), "SynqraEmergency.log"))).Contains(keyData);
+		await Assert.That(FileReadAllText(Path.Combine(Path.GetTempPath(), "Synqra", "Emergency.log"))).Contains(keyData);
+	}
+
+	[Test]
+	public async Task Should_survive_multithread()
+	{
+		// NOTE: This test can fail if file rolls over. This will stay like this until I change my mind about rolling over between 2 files instead of one.
+		// Greenifying this test is considered as the only good reason to change the rollover logic to 2 files.
+		// An honestly it does not feel fair that EmergencyLog is as reliable as it claims. It should be as a black box for flight crash research. Chances are, people log things that can happen once a month mysteriously and hard to reproduce... And what you will say? File is too big and have to be deleted?
+
+		ConcurrentBag<Guid> guids = new ConcurrentBag<Guid>();
+
+		async Task Thread()
+		{
+			for (int i = 0; i < 1_000; i++)
+			{
+				var guid = Guid.NewGuid();
+				guids.Add(guid);
+				EmergencyLog.Default.Message("Should_survive_multithread test " + guid);
+			}
+		}
+		await Task.WhenAll(Thread(), Thread(), Thread(), Thread(), Thread());
+		var path = Path.Combine(Path.GetTempPath(), "Synqra", "Emergency.log");
+		var log = FileReadAllText(path);
+		var pathTemplate = Path.Combine(Path.GetTempPath(), "Synqra", "Emergency_{0}.log");
+		for (int i = 2; ; i++)
+		{
+			var fi = new FileInfo(string.Format(pathTemplate, i));
+			if (fi.Exists && (DateTime.UtcNow - fi.LastWriteTimeUtc).TotalHours < 1)
+			{
+				log = FileReadAllText(fi.FullName) + log;
+			}
+			else
+			{
+				break;
+			}
+
+		}
+		await Assert.That(log).Contains("Should_survive_multithread");
+		using var _ = Assert.Multiple();
+		foreach (var guid in guids)
+		{
+			await Assert.That(log).Contains(guid.ToString());
+		}
+	}
+
+	private string ReadAllText(string fileName)
+	{
+		return null;
+		// DO not lock same mutextes, because we just read here. But reader must share write access, like any log viewer, to avoid problems
+		using var file = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.Read | FileShare.Write | FileShare.Delete);
+		using var sr = new StreamReader(file);
+		return sr.ReadToEnd();
 	}
 }
