b6ba1b712a0daa8b7d6ff4ab19088755e45cfaf9
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2025-12-28 10:43:27 -0500

    Start Logging Commands

---
+++ XKit.LoggingCommands.Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿namespace XKit.LoggingCommands.Tests;
+
+public class LoggingCommandsTests
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	[Explicit("It is better to delay this feature and implement it when scripting is needed")]
+	[TestCase("Log this message")]
+	[TestCase("##vso[test]")] // mninimal VSO example
+	[TestCase("::warning::hohoho")]
+	[TestCase("##warning## hohoho")]
+	[TestCase("##upsert[id=1 def=2]")]
+	[TestCase("::command par1=val1 par2=val2:: optional body")]
+	[TestCase("::vso.command par1=val1 par2=val2:: optional body")]
+	[TestCase("::q.upsert par1=val1 par2=val2:: optional body")]
+	[TestCase("::type=q.upsert par1=val1 par2=val2 value=optionalbody::")]
+	[TestCase("<System.Upsert par1=<System.Decimal value=12> par2=val2 value=optionalbody::")]
+	[TestCase("[upsert par1=val1 par2=val2]")]
+	[TestCase("[upsert par1=val1 par2=[upser]]")]
+	[TestCase("[a=1 b=2]")] // {'a'=1, 'b'=2}
+	[TestCase("[a=1 type=upsert b=2]")] // {'a'=1, 'b'=2, 'type'='upsert'} type is descriminator
+	[TestCase("[upsert a=1 b=2]")]
+	[TestCase("[upsert a=1 b=[decimal 12]]")]
+	public void Should_10_detect_and_execute_logging_command(string line)
+	{
+		Assert.Inconclusive();
+	}
+
+	[Test]
+	public void Should_15_create_new_requark()
+	{
+		Assert.Inconclusive();
+	}
+}
---
+++ XKit.LoggingCommands/StreamLoggingCommandProcessor.cs
@@ -0,0 +1,47 @@
+﻿
+namespace XKit.LoggingCommands;
+
+public class StreamLoggingCommandProcessor : System.IO.Stream
+{
+	private readonly Stream _inputStream;
+
+	public StreamLoggingCommandProcessor(Stream inputStream)
+	{
+		_inputStream = inputStream;
+	}
+
+	public override bool CanRead => _inputStream.CanRead;
+
+	public override bool CanSeek => _inputStream.CanSeek;
+
+	public override bool CanWrite => _inputStream.CanWrite;
+
+	public override long Length => _inputStream.Length;
+
+	public override long Position { get => _inputStream.Position; set => _inputStream.Position = value; }
+
+	public override void Flush()
+	{
+		_inputStream.Flush();
+	}
+
+	public override int Read(byte[] buffer, int offset, int count)
+	{
+		return _inputStream.Read(buffer, offset, count);
+	}
+
+	public override long Seek(long offset, SeekOrigin origin)
+	{
+		return _inputStream.Seek(offset, origin);
+	}
+
+	public override void SetLength(long value)
+	{
+		_inputStream.SetLength(value);
+	}
+
+	public override void Write(byte[] buffer, int offset, int count)
+	{
+		_inputStream.Write(buffer, offset, count);
+	}
+}
