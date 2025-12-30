4073b727d97cf90cfa70c11a6ae876bdcaec7629
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2025-12-30 16:51:25 -0500

    Move tests

--- Synqra.Utils.Tests/MongoDbUuidOrdering.cs
+++ Synqra.Utils.Tests/MongoDbUuidOrdering.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TUnit.Assertions.Extensions;
 
-namespace Synqra.Tests.Integration;
+namespace Synqra.Utils.Tests;
 
 [Property("CI", "false")]
 internal class MongoDbUuidOrdering : BaseTest
