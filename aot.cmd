413512606c791621b87f61c9f05919603bf3ffe8
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2026-02-03 14:39:25 -0500

    Net 10

--- aot.cmd
+++ aot.cmd
@@ -1,7 +1,7 @@
-del Tests\Synqra.Tests\bin\Release\net9.0\win-x64\publish\Synqra.Tests.exe
-dotnet publish Tests/Synqra.Tests -c Release -r win-x64 -f net9.0
+del Tests\Synqra.Tests\bin\Release\net10.0\win-x64\publish\Synqra.Tests.exe
+dotnet publish Tests/Synqra.Tests -c Release -r win-x64 -f net10.0
 if errorlevel 1 exit /b 1
-::Tests\Synqra.Tests\bin\Release\net9.0\win-x64\publish\Synqra.Tests.exe --treenode-filter /*/*/PerformanceTests/*
-::Tests\Synqra.Tests\bin\Release\net9.0\win-x64\publish\Synqra.Tests.exe 
-Tests\Synqra.Tests\bin\Release\net9.0\win-x64\publish\Synqra.Tests.exe --treenode-filter "/*/*/*[(Category!=Performance)&(CI!=false)]/*[(Category!=Performance)&(CI!=false)]"
+::Tests\Synqra.Tests\bin\Release\net10.0\win-x64\publish\Synqra.Tests.exe --treenode-filter /*/*/PerformanceTests/*
+::Tests\Synqra.Tests\bin\Release\net10.0\win-x64\publish\Synqra.Tests.exe 
+Tests\Synqra.Tests\bin\Release\net10.0\win-x64\publish\Synqra.Tests.exe --treenode-filter "/*/*/*[(Category!=Performance)&(CI!=false)]/*[(Category!=Performance)&(CI!=false)]"
 if errorlevel 1 exit /b 1
