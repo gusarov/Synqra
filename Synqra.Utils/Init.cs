67791cf9932a8d7c4cf7113e73cdd4b28ab3d82b
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2026-03-12 21:10:11 -0400

    Minor

---
+++ Synqra.Utils/Init.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Synqra;
+
+interface IInit
+{
+	void Init();
+}
+
+internal class Init : IInit
+{
+	void IInit.Init()
+	{
+		throw new NotImplementedException();
+	}
+}
